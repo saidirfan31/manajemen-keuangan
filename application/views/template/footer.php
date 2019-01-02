37f399a29b9c01fac90e8427ff2eb520f47a6aaa
Muhammad Said Marzuqi <saidirfan31@gmail.com>
2019-01-02 07:01:40 +0700

    Update

---
+++ application/views/template/footer.php
@@ -0,0 +1,11 @@
+
+	<footer id="footer">
+		<p>&copy <?=date('Y');?> - KasApp | Dibuat oleh <a href="https://www.bablog.us" target="_blank">Bablog.us</a></p>
+    </footer>
+</div>
+<script src="<?=base_url('assets/vendor/jquery/dist/jquery.min.js');?>"></script>
+<script src="<?=base_url('assets/vendor/propper.js/dist/umd/popper.min.js');?>"></script>
+<script src="<?=base_url('assets/vendor/bootstrap/dist/js/bootstrap.min.js');?>"></script>
+<script src="<?=base_url('assets/js/custom.js');?>"></script>
+</body>
+</html>
